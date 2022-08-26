0a158bfa962ce23c8a89c76bc5b8b9c4338c2ff9
Javier <u20191e814@upc.edu.pe>
2022-08-25 22:50:25 -0500

    Initial commit

---
+++ src/main/java/corredor/backend/jhuarancca2022/Jhuarancca2022Application.java
@@ -0,0 +1,13 @@
+package corredor.backend.jhuarancca2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jhuarancca2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jhuarancca2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/corredor/backend/jhuarancca2022/Jhuarancca2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package corredor.backend.jhuarancca2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jhuarancca2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
